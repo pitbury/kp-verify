e31415c028b09c97690c846c4186fa76f91f095b
ppburylo <pitbury111@gmail.com>
2026-08-11 21:36:23 +0200

    Update ecpp.py

--- ecpp.py
+++ ecpp.py
@@ -34,7 +34,7 @@ CHAIN CLOSURE
     Anything else is rejected. A chain that merely ends in a probable prime is
     not a proof and this module will not say that it is.
 
-Copyright (c) 2026 Piotr Burczynski. MIT License.
+Copyright (c) 2026 IDEALPLACE algorithms. MIT License.
 """
 import gmpy2
 from gmpy2 import mpz, gcd, isqrt, invert
