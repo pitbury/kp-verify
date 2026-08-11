0ae7c4ea63aa92b4806278e75fde30ae52fa7733
ppburylo <pitbury111@gmail.com>
2026-08-11 22:12:36 +0200

    Update kp-verify.js

--- kp-verify.js
+++ kp-verify.js
@@ -12,7 +12,7 @@
  * projective coordinates so that no modular inverse is needed per operation,
  * which is what makes 4096-bit certificates practical in a browser.
  *
- * Copyright (c) 2026 IDEALPLACE algorithms. MIT License.
+ * Copyright (c) 2026 Piotr Burylo. MIT License.
  */
 (function (root, factory) {
   if (typeof module === "object" && module.exports) module.exports = factory();
